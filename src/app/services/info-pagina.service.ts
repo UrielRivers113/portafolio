1d80cb8fdce984ac200eeafd1bf111c76647ff47
Uriel Ríos <urielriosmartinez93@gmail.com>
2020-05-11 01:35:15 -0500

    Fin sección 6

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -9,17 +9,27 @@ export class InfoPaginaService {
 
   info: InfoPagina = {};
   cargada = false;
+  equipo: any[] = [];
 
   constructor(private http: HttpClient) { 
-    //console.log("Servicio de infoPagina listo");
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
+
+  private cargarInfo(){
     // Leer el archivo Json
     this.http.get('assets/data/data-pagina.json')
     .subscribe((resp:InfoPagina)=>{
       this.cargada = true;
       this.info = resp;
-      console.log(resp);
-
     });
+  }
 
+  private cargarEquipo(){
+    // Leer el archivo Json
+    this.http.get('https://portafolio-b412e.firebaseio.com/equipo.json')
+    .subscribe((resp: any[])=>{
+      this.equipo = resp;
+    });
   }
 }
